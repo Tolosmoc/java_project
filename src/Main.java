dced804592ea4360fe04224bbba10174d37c1d32
Tolosmoc <54775291+Tolosmoc@users.noreply.github.com>
2023-05-07 21:16:54 +0200

    Update Main.java
    Devrait fonctionner via le GUI directement

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        /*
         String source = "C:\\Users\\pgien\\Documents\\Cours\\Semestre 6\\Object-Oriented Programming with Java\\Projet Java\\java_project\\.debug\\source";
         String target = "C:\\Users\\pgien\\Documents\\Cours\\Semestre 6\\Object-Oriented Programming with Java\\Projet Java\\java_project\\.debug\\target";
 
@@ -17,11 +18,11 @@ public class Main {
 
         sync1.start();
         sync2.start();
+        */
         
-        /*
-        // to show the GUI:
+        
         Menu menu = new Menu();
         menu.Menu();
-        */
+        
     }
 }
