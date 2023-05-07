ab868066f813a225123c1e05dcef90b53ef55b3d
Tolosmoc <54775291+Tolosmoc@users.noreply.github.com>
2023-05-07 11:05:10 +0200

    Update Software.java
    Suppresion du "main" dans cette classe.

--- src/Swing/Software.java
+++ src/Swing/Software.java
@@ -8,8 +8,8 @@ public class Software extends javax.swing.JFrame {
     
     public Software() {
         initComponents();
-        
         cardLayout = (CardLayout)(CardTab.getLayout());
+        this.setVisible(true);
     }
 
     @SuppressWarnings("unchecked")
@@ -233,32 +233,6 @@ public class Software extends javax.swing.JFrame {
     
     private void ExitActionPerformed(java.awt.event.ActionEvent evt) {
         System.exit(0);
-    }
-
-    public static void main(String args[]) {
-
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Software.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Software.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Software.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Software.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Software().setVisible(true);
-            }
-        });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
