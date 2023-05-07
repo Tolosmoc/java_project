912c238d57ad05a533d8a7c8e0d97fc0bc9f880b
Tolosmoc <hectochien@gmail.com>
2023-05-07 23:40:09 +0200

    end ?

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import Swing.Menu;
 
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args){
         Menu menu = new Menu();
         menu.Menu();
     }
